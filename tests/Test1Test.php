d21f56d880cfc8cb204bb3a4331a3bbd217caddf
Amashige Seiji <tenjuu99@gmail.com>
2020-05-04 00:05:25 +0900

    add test2

--- tests/Test1Test.php
+++ tests/Test1Test.php
@@ -16,4 +16,15 @@ class Test1Test extends TestCase
         $pc->setCategory($electronic);
         $this->assertEquals($electronic, $pc->getCategory());
     }
+
+    /**
+     * @test
+     */
+    public function test2() : void
+    {
+        $pc = new Product();
+        $electronic = new Category();
+        $pc->setCategory($electronic);
+        $this->assertTrue($electronic->contains($pc));
+    }
 }
